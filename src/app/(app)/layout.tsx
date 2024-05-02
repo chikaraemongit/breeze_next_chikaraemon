c705edbfe4315512530988dfc4ea9f21995bcc85
chikaraemongit <gituser@chikaraemon.com>
2024-05-03 08:56:11 +0900

    laravel breeze next

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useAuth } from '@/hooks/auth'
+import Navigation from '@/app/(app)/Navigation'
+import Loading from './Loading'
+
+const AppLayout = ({ children, header }) => {
+    const { user } = useAuth({ middleware: 'auth' })
+
+    if (!user) {
+        return <Loading />
+    }
+
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Navigation user={user} />
+
+            <main>{children}</main>
+        </div>
+    )
+}
+
+export default AppLayout
---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import './global.css'
+
+export const metadata = {
+    title: 'Laravel',
+}
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body className="antialiased">{children}</body>
+        </html>
+    )
+}
+
+export default RootLayout
